0a4cc76ea33d9c57a9bd99ef31833887a3a5d6c2
NeslyDajui <nesly_yohary@hotmail.com>
2019-04-04 08:35:13 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+import TweetBox from './TweetBox';
+import Feed from './Feed';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      tweets: [],
+      error: null,
+      isLoaded: true
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://still-garden-88285.herokuapp.com/draft_tweets")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            tweets: result.draft_tweets
+          })
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error: error
+          })
+        }
+      )
+  }
+
+  handleSubmit(newText) {
+    this.setState({isLoaded: false});
+
+    // Static UI :(
+    // let newTweet = {
+    //   id: this.state.tweets.length + 1,
+    //   user_name: 'Yvone',
+    //   avatar: 'https://img.ifcdn.com/images/d3951bf44788590b80f69c0c65718f7a23eb33c645cb677ee335f81a6e785ee6_3.jpg',
+    //   created_at: '11-03-2019',
+    //   description: newText
+    // };
+
+    // let tweets = this.state.tweets.slice();
+
+    // this.setState({ tweets: tweets.concat(newTweet) });
+
+    // Dynamic UI !
+    let newTweet = {
+      user_name: 'Yvone',
+      avatar: 'https://img.ifcdn.com/images/d3951bf44788590b80f69c0c65718f7a23eb33c645cb677ee335f81a6e785ee6_3.jpg',
+      description: newText
+    };
+
+    let headers = {};
+    headers['Content-Type'] = 'application/json';
+
+    const options = {
+      headers: headers,
+      method: 'POST',
+      // credentials: 'include',
+      body: JSON.stringify(newTweet)
+    };
+
+    fetch("https://still-garden-88285.herokuapp.com/draft_tweets", options)
+      .then(res => res.json())
+      .then(result => (
+        this.setState({ 
+          tweets: result.draft_tweets, 
+          isLoading: false }), 
+        console.log(this.state))
+      );
+  }
+
+  handleMouseOutDrop(newText){
+    newText.target.classList.remove("show", "active");
+  }
+
+  deleteTweet(newText){
+    console.log(newText.target.getAttribute('result'));
+    let newTweet = newText.target.getAttribute('result');
+    fetch('https://still-garden-88285.herokuapp.com/draft_tweets/${newTweet}', {
+    method: 'DELETE',
+    })
+    .then(this.setState({isLoaded: true}))
+    .then(setTimeout(this.componentDidMount.bind(this), 500));
+  }
+
+  render() {
+    const { error, isLoaded, tweets } = this.state;
+    let content;
+
+    if (error) {
+      content = <div>Error: {error.message}</div>;
+    } else {
+      return (
+        content = (
+          <Fragment>
+            <TweetBox
+              onSubmitNewTweet={this.handleSubmit}
+            />
+            <Feed 
+              tweets={tweets}
+              isLoaded={isLoaded}
+            />
+          </Fragment>
+        )
+      );
+    }
+
+    return (
+      <div className="App">
+        { content }
+      </div>
+    )
+  }
+}
+
+export default App;
